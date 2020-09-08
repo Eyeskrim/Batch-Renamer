ec2cccf2340b2e537d1e38f72c4be6e2dc295dcc
Yzvanne Guerrero <yzvanneg@gmail.com>
2020-09-08 21:02:01 +0800

    Add files

---
+++ BatchRenamer/BatchRenamer/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace BatchRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtRootPath = new System.Windows.Forms.TextBox();
+            this.grpInfo = new System.Windows.Forms.GroupBox();
+            this.lblNoFiles = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblExtensions = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNewName = new System.Windows.Forms.TextBox();
+            this.grpInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(340, 21);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(89, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Set Root Path";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtRootPath
+            // 
+            this.txtRootPath.Location = new System.Drawing.Point(12, 21);
+            this.txtRootPath.Name = "txtRootPath";
+            this.txtRootPath.ReadOnly = true;
+            this.txtRootPath.Size = new System.Drawing.Size(322, 20);
+            this.txtRootPath.TabIndex = 1;
+            // 
+            // grpInfo
+            // 
+            this.grpInfo.Controls.Add(this.lblExtensions);
+            this.grpInfo.Controls.Add(this.label6);
+            this.grpInfo.Controls.Add(this.lblNoFiles);
+            this.grpInfo.Controls.Add(this.label4);
+            this.grpInfo.Location = new System.Drawing.Point(13, 81);
+            this.grpInfo.Name = "grpInfo";
+            this.grpInfo.Size = new System.Drawing.Size(416, 96);
+            this.grpInfo.TabIndex = 3;
+            this.grpInfo.TabStop = false;
+            this.grpInfo.Text = "Information";
+            // 
+            // lblNoFiles
+            // 
+            this.lblNoFiles.AutoSize = true;
+            this.lblNoFiles.Location = new System.Drawing.Point(128, 26);
+            this.lblNoFiles.Name = "lblNoFiles";
+            this.lblNoFiles.Size = new System.Drawing.Size(0, 13);
+            this.lblNoFiles.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "No. of Files:";
+            // 
+            // lblExtensions
+            // 
+            this.lblExtensions.AutoSize = true;
+            this.lblExtensions.Location = new System.Drawing.Point(128, 56);
+            this.lblExtensions.Name = "lblExtensions";
+            this.lblExtensions.Size = new System.Drawing.Size(0, 13);
+            this.lblExtensions.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "File Extension(s):";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(268, 183);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(353, 183);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "File Name";
+            // 
+            // txtNewName
+            // 
+            this.txtNewName.Location = new System.Drawing.Point(72, 53);
+            this.txtNewName.Name = "txtNewName";
+            this.txtNewName.Size = new System.Drawing.Size(357, 20);
+            this.txtNewName.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 215);
+            this.Controls.Add(this.txtNewName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.grpInfo);
+            this.Controls.Add(this.txtRootPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Batch Renamer";
+            this.grpInfo.ResumeLayout(false);
+            this.grpInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtRootPath;
+        private System.Windows.Forms.GroupBox grpInfo;
+        private System.Windows.Forms.Label lblNoFiles;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblExtensions;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNewName;
+    }
+}
+
---
+++ BatchRenamer/BatchRenamer/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BatchRenamer
+{
+    public partial class Form1 : Form
+    {
+        private string _rootPath;
+        private int _renameCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult res = fbd.ShowDialog();
+
+                if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    _rootPath = fbd.SelectedPath;
+                    txtRootPath.Text = fbd.SelectedPath;
+
+                    lblNoFiles.Text = FileCount().ToString();
+                    lblExtensions.Text = String.Join(",", GetFileExtensions(GetFiles(_rootPath)).Distinct());
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_rootPath))
+            {
+                MessageBox.Show("Set root path.", Application.ProductName, MessageBoxButtons.OK);
+                txtRootPath.Focus();
+
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtNewName.Text))
+            {
+                MessageBox.Show("Set new file name.", Application.ProductName, MessageBoxButtons.OK);
+                txtNewName.Focus();
+
+                return;
+            }
+
+            if (!RenameFiles(txtNewName.Text, GetFiles(_rootPath)))
+            {
+                MessageBox.Show("Something happened.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            MessageBox.Show($"Successfully renamed {_renameCount} files.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ClearValues();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ClearValues()
+        {
+            _rootPath = "";
+            _renameCount = 0;
+            txtRootPath.Text = "";
+            txtNewName.Text = "";
+        }
+
+        private int FileCount()
+        {
+            return GetFiles(_rootPath).Length + 1;
+        }
+
+        private string[] GetFiles(string rootPath)
+        {
+            string[] files = Directory.GetFiles(rootPath);
+
+            return files;
+        }
+
+        private List<string> GetFileExtensions(string[] filePaths)
+        {
+            List<string> extensions = new List<string>();
+
+            filePaths.ToList().ForEach(file => extensions.Add(Path.GetExtension(file)));
+
+            return extensions;
+        }
+
+        private bool RenameFiles(string fileName, string[] files)
+        {
+            int start = 1;
+
+            foreach (var file in files)
+            {
+                string newTitle = fileName + start.ToString("D2");
+                string directoryName = Path.GetDirectoryName(file);
+                string newFile = Path.Combine(directoryName, newTitle + Path.GetExtension(file));
+
+                try
+                {
+                    File.Move(file, newFile);
+                    _renameCount++;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                    return false;
+                }
+
+                start++;
+            }
+
+            return true;
+        }
+    }
+}
